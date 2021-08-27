9a51a8a5d4e2fd53016b6701fce89eca4bd26f0d
Tobies <37544558+Tobies@users.noreply.github.com>
2021-08-27 01:11:52 -0700

    Update main.js

--- main.js
+++ main.js
@@ -18,9 +18,14 @@ function scannerClick() {
 }
 
 function mapClick() {
-
+    document.getElementById("map-container").style = "position:fixed;padding:10px;padding-bottom: 1vh;border-radius: 15px;background-color:#1f1f1f;top:1vh;text-align: center;width:91vw;"
+}
+function closeMap() {
+    document.getElementById("map-container").style = "visibility: hidden;position:fixed;padding:10px;padding-bottom: 1vh;border-radius: 15px;background-color:#1f1f1f;top:1vh;text-align: center;width:91vw;"
+    
 }
 
 document.getElementById("BTN-INDEXER").onclick = indexerClick;
 document.getElementById("BTN-SCANNER").onclick = scannerClick;
 document.getElementById("BTN-MAP").onclick = mapClick;
+document.getElementById("map-btn").onclick = closeMap;
