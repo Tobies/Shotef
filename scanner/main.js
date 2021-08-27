69a81914618283c8d4fa3ca7c668d3522dbea950
Tobies <37544558+Tobies@users.noreply.github.com>
2021-08-27 05:13:14 -0700

    Update main.js

--- scanner/main.js
+++ scanner/main.js
@@ -1,11 +1,8 @@
 
 function onScanSuccess(decodedText, decodedResult) {
     if (decodedText.includes("https://tobies.github.io/IDF-QR-THINGY/index.html#data=")) {
-        html5QrcodeScanner.clear();
-        location.href = "./orders-manager/index.html#data=" + decodedText.replace("https://tobies.github.io/IDF-QR-THINGY/index.html#data=", "")
-        
+        location.href("./orders-manager/index.html#data=" + decodedText.replace("https://tobies.github.io/IDF-QR-THINGY/index.html#data=", ""))
     } else {
-        
         console.log("INVALID CODE! " + decodedText)
     }
 }
@@ -20,17 +17,35 @@ function findButton(context) {
     return null;
 }
 
-function applyStyle() {
-    console.log("style")
-    document.getElementById("qr-reader").style="width:80vw;height:80vw;margin-left: 8vw;margin-top:50px;border-radius: 15px;background-color: #EEEEFF;border:0px transparent;padding: 0px;";
-    document.getElementById("qr-reader__dashboard_section_csr").style = "display: block;text-align: center;background-color: #EEEEFF; border-radius: 0px 0px 15px 15px;";
+function startScan() {
+    html5QrcodeScanner.html5Qrcode.start({ facingMode: "environment" }, { fps: 10, qrbox: 250, aspectRatio:1.0}, onScanSuccess)
+    document.getElementById("start-btn").onclick = stopScan;
+    document.getElementById("start-btn").textContent = "הפסק לסרוק"
 }
 
-var html5QrcodeScanner = new Html5QrcodeScanner("qr-reader", { fps: 10, qrbox: 250});
+function stopScan() {
+    html5QrcodeScanner.html5Qrcode.stop();
+    document.getElementById("start-btn").onclick = startScan;
+    document.getElementById("start-btn").textContent = "התחל לסרוק"
+}
+
+function applyStyle() {
+    document.getElementById("qr-reader").style="width:80vw;height:80vw;margin-left: 10vw;margin-top:50px;border-radius: 25px;background-color: #1C1C1C;border:0px transparent;padding: 0px;";
+    document.getElementById("qr-reader__dashboard_section_csr").style="display:none;"
+    document.getElementById("start-btn").style="border-radius: 25px 25px 0px 0px; border-color: transparent; background-color: #60C8D9; position:fixed; bottom:0px; width:100vw; left:0px; padding-bottom:30px; padding-top:30px; font-family: 'Rubik', sans-serif; font-size:5vw; color:#121212; text-align: center";
+    document.getElementById("start-btn").onclick = startScan;
+}
+
+
+
+var html5QrcodeScanner = new Html5QrcodeScanner("qr-reader", { fps: 10, qrbox: 250, aspectRatio:1.0});
 html5QrcodeScanner.render(onScanSuccess);
-document.getElementById("qr-reader").style="width:80vw;height:80vw;margin-left: 8vw;margin-top:50px;border-radius: 25px;background-color: #1C1C1C;border:0px transparent;padding: 0px;";
-document.getElementById("qr-reader__status_span").style="padding: 5px 7px;font-size: 14px;background: transparent none repeat scroll 0% 0%;border: 1px solid rgba(0, 0, 0, 0);color: rgb(17, 17, 17);float:left; color: white;";
+document.getElementById("qr-reader").style="width:80vw;height:80vw;margin-left: 10vw;margin-top:50px;border-radius: 25px;background-color: #1C1C1C;border:0px transparent;padding: 0px;";
+document.getElementById("qr-reader__status_span").hidden = true;
 document.getElementById("qr-reader__scan_region").style="width: 100%;min-height: 20vw;text-align: center;"
-findButton("Request Camera Permissions").style="border-radius: 10px; border-color: transparent; padding:5px; background-color: #60C8D9; margin-top:25vw"
+findButton("Request Camera Permissions").style="border-radius: 25px 25px 0px 0px; border-color: transparent; padding:5px; background-color: #60C8D9; position:fixed; bottom:0px; width:100vw; left:0px; padding-bottom:30px; padding-top:30px; font-family: 'Rubik', sans-serif; font-size:5vw; color:#121212"
 findButton("Request Camera Permissions").onclick = applyStyle;
+findButton("Request Camera Permissions").textContent = "בקש גישה לשימוש במצלמה"
+html5QrcodeScanner.cameraScanImage.style = "opacity:0.1; width:100%; height:100%;"
+html5QrcodeScanner.cameraScanImage.src = "https://freefrontend.com/assets/img/css-arrows/simple-arrow-animation.gif"
 document.getElementById("qr-reader__dashboard_section").style="";
