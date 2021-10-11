220b857d3c900f8ff7b7940a55e25fea1a30b9b6
Tobies <37544558+Tobies@users.noreply.github.com>
2021-10-11 09:00:24 -0700

    Add files via upload

--- manual/main.js
+++ manual/main.js
@@ -2,6 +2,8 @@ function navigate() {
     var value =  document.getElementById("INPUT-FIELD").value;
     if (value.includes("https://tobies.github.io/IDF-QR-THINGY/index.html#data=")) {
         location.href = "../scanner/orders-manager/index.html#data=" + value.replace("https://tobies.github.io/IDF-QR-THINGY/index.html#data=", "");
+    } else if (value.includes("https://tobies.github.io/Shotef/scanner/orders-manager/index.html#data=")) {
+        location.href = "../scanner/orders-manager/index.html#data=" + value.replace("https://tobies.github.io/Shotef/scanner/orders-manager/index.html#data=", "");
     } else {
         alert("הקישור שהוזן לא תקין")
     }
